28a4857a6b7164ad8807046c8cdbed56e1bd6ea2
octopusbob <bob.walker@octopus.com>
2020-10-05 14:51:50 -0500

    Adding ability to lock down environments/tenants if the account is new

--- azure/bootstrap-azure.ps1
+++ azure/bootstrap-azure.ps1
@@ -2,6 +2,7 @@ param (
     $OctopusURL,
     $OctopusApiKey,
     $OctopusSpaceName,
+    $OctopusAccountName,
     $AzureTenantId,
     $AzureSubscriptionName,
     $AzureServicePrincipalName
@@ -46,6 +47,25 @@ function Get-ParameterValue
     return Read-Host -Prompt "Please enter a value for $parameterName"
 }
 
+function Get-ParameterValueWithDefault
+{
+    param
+    (
+        $originalParameterValue,
+        $parameterName,
+        $defaultValue
+    )
+
+    $returnValue = Get-ParameterValue -originalParameterValue $originalParameterValue -parameterName $parameterName
+
+    if ([string]::IsNullOrWhiteSpace($returnValue) -eq $true)
+    {
+        return $defaultValue
+    }
+
+    return $returnValue
+}
+
 function Invoke-OctopusApi
 {
     param
@@ -69,37 +89,16 @@ function Invoke-OctopusApi
         $url = "$OctopusUrl/api"
     }
 
-    try
-    {
-        if ($null -eq $item)
-        {           
-            Write-OctopusVerbose "Invoking GET $url" 
-            return Invoke-RestMethod -Method $method -Uri $url -Headers @{"X-Octopus-ApiKey" = "$ApiKey" } -ContentType 'application/json; charset=utf-8'
-        }
-
-        $body = $item | ConvertTo-Json -Depth 10        
-
-        Write-OctopusVerbose "Invoking $method $url"
-        return Invoke-RestMethod -Method $method -Uri $url -Headers @{"X-Octopus-ApiKey" = "$ApiKey" } -Body $body -ContentType 'application/json; charset=utf-8'
-    }
-    catch
-    {
-        if ($null -ne $_.Exception.Response)
-        {
-            $result = $_.Exception.Response.GetResponseStream()
-            $reader = New-Object System.IO.StreamReader($result)
-            $reader.BaseStream.Position = 0
-            $reader.DiscardBufferedData()
-            $responseBody = $reader.ReadToEnd();
-            Write-OctopusVerbose -Message "Error calling $url $($_.Exception.Message) StatusCode: $($_.Exception.Response.StatusCode.value__ ) StatusDescription: $($_.Exception.Response.StatusDescription) $responseBody"
-        }
-        else
-        {
-            Write-OctopusVerbose $_.Exception
-        }
+    if ($null -eq $item)
+    {           
+        Write-OctopusVerbose "Invoking GET $url" 
+        return Invoke-RestMethod -Method $method -Uri $url -Headers @{"X-Octopus-ApiKey" = "$ApiKey" } -ContentType 'application/json; charset=utf-8'
     }
 
-    Throw "There was an error calling the Octopus API please check the log for more details"
+    $body = $item | ConvertTo-Json -Depth 10        
+
+    Write-OctopusVerbose "Invoking $method $url"
+    return Invoke-RestMethod -Method $method -Uri $url -Headers @{"X-Octopus-ApiKey" = "$ApiKey" } -Body $body -ContentType 'application/json; charset=utf-8'
 }
 
 function Get-OctopusItemByName
@@ -112,58 +111,55 @@ function Get-OctopusItemByName
     return ($ItemList | Where-Object {$_.Name -eq $ItemName})
 }
 
-Write-OctopusVerbose "This script will do the following:"
-Write-OctopusVerbose "    1) In Azure: create an Azure Service Principal and associate it with your desired subscription as a contributor"
-Write-OctopusVerbose "    2) In Octopus Deploy: create an Azure Account using the credentials created in step 1"
-
-Write-OctopusVerbose "For this to work you will need to have the following installed.  If it is not installed, then this script will it install it for you from the PowerShell Gallery."
-Write-OctopusVerbose "    1)  Azure Az Powershell Modules"
-
-$answer = Read-Host -Prompt "Do you wish to continue? y/n"
-if ($answer.ToLower() -ne "y")
+function Import-AzurePowerShellModules
 {
-    Write-OctopusWarning "You have chosen not to continue.  Stopping script."
-    Exit
+    if (Get-Module -Name Az -ListAvailable)
+    {    
+        Write-OctopusVerbose "Azure Az Module found."
+    }
+    else
+    {
+        Write-OctopusVerbose "Azure Az Modules not found.  Installing the Azure Az PowerShell Modules.  You might be prompted that PSGallery is untrusted.  If you select Yes your screen might freeze for a second while the modules download process is started."
+        Install-Module -Name Az -AllowClobber -Scope CurrentUser
+    }
+
+    Write-OctopusVerbose "Loading the Azure Az Module"
+    Import-Module -Name Az
 }
 
-if (Get-Module -Name Az -ListAvailable)
-{    
-    Write-OctopusVerbose "Azure Az Module found."
-}
-else
+function Get-OctopusSpaceInformation
 {
-    Write-OctopusVerbose "Azure Az Modules not found.  Installing the Azure Az PowerShell Modules.  You might be prompted that PSGallery is untrusted.  If you select Yes your screen might freeze for a second while the modules download process is started."
-    Install-Module -Name Az -AllowClobber -Scope CurrentUser
-}
+    param (
+        $OctopusApiKey,
+        $OctopusUrl,
+        $OctopusSpaceName
+    )
 
-Write-OctopusVerbose "Loading the Azure Az Module"
-Import-Module -Name Az
+    Write-OctopusVerbose "Testing the API crendentials of the credentials supplied by pulling the space information"
+    $spaceResults = Invoke-OctopusApi -EndPoint "spaces?skip=0&take=100000" -SpaceId $null -OctopusURL $OctopusURL -apiKey $OctopusApiKey -method "Get" -item $null
+    $spaceInfo = Get-OctopusItemByName -ItemList $spaceResults.Items -ItemName $OctopusSpaceName
 
-$OctopusURL = Get-ParameterValue -originalParameterValue $OctopusURL -parameterName "the URL of your Octopus Deploy Instance, example: https://samples.octopus.com"
-$OctopusApiKey = Get-ParameterValue -originalParameterValue $OctopusApiKey -parameterName "the API Key of your Octopus Deploy User.  See https://octopus.com/docs/octopus-rest-api/how-to-create-an-api-key for a guide on how to create one"
-$OctopusSpaceName = Get-ParameterValue -originalParameterValue $OctopusSpaceName -parameterName "the name of the space in Octopus Deploy.  Enter Default if unsure"
-
-Write-OctopusVerbose "Testing the API crendentials of the credentials supplied by pulling the space information"
-$spaceResults = Invoke-OctopusApi -EndPoint "spaces?skip=0&take=100000" -SpaceId $null -OctopusURL $OctopusURL -apiKey $OctopusApiKey -method "Get" -item $null
-$spaceInfo = Get-OctopusItemByName -ItemList $spaceResults.Items -ItemName $OctopusSpaceName
-if ($null -ne $spaceInfo -and $null -ne $spaceInfo.Id)
-{
-    Write-OctopusSuccess "Successfully connected to the Octopus Deploy instance provided.  The space id for $OctopusSpaceName is $($spaceInfo.Id)"
-}
-else
-{
-    Write-OctopusWarning "Unable to connect to $OctopusUrl.  Please check your credentials and try again."
-    exit 1
+    if ($null -ne $spaceInfo -and $null -ne $spaceInfo.Id)
+    {
+        Write-OctopusSuccess "Successfully connected to the Octopus Deploy instance provided.  The space id for $OctopusSpaceName is $($spaceInfo.Id)"
+        return $spaceInfo
+    }
+    else
+    {
+        Write-OctopusWarning "Unable to connect to $OctopusUrl.  Please check your credentials and try again."
+        exit 1
+    }    
 }
 
-$accountName = "Bootstrap Azure Account"
-
-$existingOctopusAccounts = Invoke-OctopusApi -EndPoint "accounts?skip=0&take=1000000" -method "GET" -SpaceId $spaceInfo.Id -apiKey $OctopusApiKey -OctopusURL $OctopusURL
-$existingAccount = Get-OctopusItemByName -ItemList $existingOctopusAccounts.Items -ItemName $accountName
-$OctopusAndAzureServicePrincipalAlreadyExist = $false
-
-if ($null -ne $existingAccount)
+function Test-ExistingOctopusAccountWorksWithAzure
 {
+    param (
+        $OctopusApiKey,
+        $OctopusUrl,
+        $SpaceInfo,
+        $ExistingAccount
+    )
+
     Write-OctopusVerbose "The account already exists in Octopus Deploy.  Running a test to ensure it can connect to Azure."
     $testAccountTaskBody = @{
         "Name" = "TestAccount"
@@ -182,7 +178,7 @@ if ($null -ne $existingAccount)
 
     While ($taskDone -eq $false)
     {
-        Write-OctopusVerbose "Checking on the status of the task in 3 seconds."
+        Write-OctopusVerbose "Checking on the status of the task in 3 seconds"
         Start-Sleep -Seconds 3
         $taskStatus = Invoke-OctopusApi -EndPoint $taskStatusEndPoint -SpaceId $null -OctopusURL $OctopusURL -apiKey $OctopusApiKey -method "GET"        
         $taskState = $taskStatus.State
@@ -192,10 +188,103 @@ if ($null -ne $existingAccount)
 
         if ($taskState -eq "Success")
         {
-            $OctopusAndAzureServicePrincipalAlreadyExist = $true
-            Write-OctopusSuccess "The Octopus Account can successfully connect to Azure, skipping."
+            Write-OctopusSuccess "The Octopus Account can successfully connect to Azure"
+            return $true            
+        }        
+    } 
+
+    return $false
+}
+
+function New-OctopusIdList
+{
+    param (
+        $OctopusUrl,
+        $OctopusApiKey,
+        $spaceInfo,
+        $endPoint,
+        $itemName
+    )
+
+    Write-OctopusVerbose "Checking to see if Octopus Deploy instance has $itemName"
+    $allItemsList = Invoke-OctopusApi -EndPoint "$($endPoint)?skip=0&take=100000" -method "Get" -SpaceId $spaceInfo.Id -OctopusURL $OctopusUrl -apiKey $OctopusApiKey
+    $IdList = @()
+
+    if ($allItemsList.Items.Count -le 0)
+    {
+        return $IdList
+    }
+
+    $itemFilter = Read-Host -prompt "$itemName records found.  Please enter a comman-separated list of $itemName you'd like to associate the account to.  If left blank the account can be used for all $itemName."        
+    
+    if ([string]::IsNullOrWhiteSpace($itemFilter) -eq $true)
+    {
+        return $IdList
+    }
+
+    $itemList = $itemFilter -split ","
+    foreach ($item in $itemList)
+    {
+        $foundItem = Get-OctopusItemByName -ItemList $allItemsList.Items -ItemName $item
+
+        if ($null -eq $foundItem)
+        {
+            Write-OctopusWarning "The $itemName $item was not found in your Octopus Deploy instance."
+            $continue = Read-Host -Prompt "Would you like to continue?  If yes, the account will not be tied to $itemName $item.  y/n"
+            if ($continue.ToLower() -ne "y")
+            {
+                exit
+            }
         }
-    }    
+        else 
+        {
+            $IdList += $foundItem.Id    
+        }
+    }
+
+    return $IdList
+}
+
+Write-OctopusVerbose "This script will do the following:"
+Write-OctopusVerbose "    1) In Azure: create an Azure Service Principal and associate it with your desired subscription as a contributor.  The password generated is two GUIDs without dashes."
+Write-OctopusVerbose "    2) In Octopus Deploy: create an Azure Account using the credentials created in step 1"
+
+Write-OctopusVerbose "For this to work you will need to have the following installed.  If it is not installed, then this script will it install it for you from the PowerShell Gallery."
+Write-OctopusVerbose "    1)  Azure Az Powershell Modules"
+
+$answer = Read-Host -Prompt "Do you wish to continue? y/n"
+if ($answer.ToLower() -ne "y")
+{
+    Write-OctopusWarning "You have chosen not to continue.  Stopping script"
+    Exit
+}
+
+Import-AzurePowerShellModules
+
+$OctopusURL = Get-ParameterValue -originalParameterValue $OctopusURL -parameterName "the URL of your Octopus Deploy Instance, example: https://samples.octopus.com"
+$OctopusApiKey = Get-ParameterValue -originalParameterValue $OctopusApiKey -parameterName "the API Key of your Octopus Deploy User.  See https://octopus.com/docs/octopus-rest-api/how-to-create-an-api-key for a guide on how to create one"
+$OctopusSpaceName = Get-ParameterValueWithDefault -originalParameterValue $OctopusSpaceName -parameterName "the name of the space in Octopus Deploy.  If left empty it will default to 'Default'" -defaultValue "Default"
+$OctopusAccountName = Get-ParameterValueWithDefault -originalParameterValue $OctopusAccountName -parameterName "the name of the account you wish to create in Octopus Deploy.  If left empty it will default to 'Bootstrap Azure Account'" -defaultValue "Bootstrap Azure Account"
+
+$spaceInfo = Get-OctopusSpaceInformation -OctopusApiKey $OctopusApiKey -OctopusUrl $OctopusURL -OctopusSpaceName $OctopusSpaceName
+
+Write-OctopusVerbose "Getting the list of accounts on that space in Octopus Deploy to see if it exists"
+$existingOctopusAccounts = Invoke-OctopusApi -EndPoint "accounts?skip=0&take=1000000" -method "GET" -SpaceId $spaceInfo.Id -apiKey $OctopusApiKey -OctopusURL $OctopusURL
+$existingAccount = Get-OctopusItemByName -ItemList $existingOctopusAccounts.Items -ItemName $OctopusAccountName
+$OctopusAndAzureServicePrincipalAlreadyExist = $false
+$OctopusEnvironmentIdList = @()
+$OctopusTenantIdList = @()
+
+if ($null -ne $existingAccount)
+{
+    $OctopusAndAzureServicePrincipalAlreadyExist = Test-ExistingOctopusAccountWorksWithAzure -OctopusApiKey $OctopusApiKey -OctopusUrl $OctopusURL -SpaceInfo $spaceInfo -ExistingAccount $existingAccount
+}
+else 
+{
+    Write-OctopusWarning "The account $OctopusAccountName does not exist.  After creating the Azure Account it will create a new account in Octopus Deploy"
+    Write-OctopusVerbose "Octopus accounts can be locked down to specific environments, tenants and tenant tags"
+    $OctopusEnvironmentIdList = New-OctopusIdList -OctopusUrl $OctopusURL -OctopusApiKey $OctopusApiKey -spaceInfo $spaceInfo -endPoint "environments" -itemName "environments"
+    $OctopusTenantIdList = New-OctopusIdList -OctopusUrl $OctopusURL -OctopusApiKey $OctopusApiKey -spaceInfo $spaceInfo -endPoint "tenants" -itemName "tenants"    
 }
 
 if ($OctopusAndAzureServicePrincipalAlreadyExist -eq $true)
@@ -203,21 +292,20 @@ if ($OctopusAndAzureServicePrincipalAlreadyExist -eq $true)
     $overwriteExisting = Read-Host -Prompt "Octopus Deploy already has a working connection with Azure.  Do you wish to continue?  This will create a new password for the service principal account in Azure and update the account in Octopus Deploy.  y/n"
     If ($overwriteExisting.ToLower() -ne "y")
     {
-        Write-OctopusSuccess "Octopus Deploy already has a working connection and you elected to leave as is, stopping script."
+        Write-OctopusSuccess "Octopus Deploy already has a working connection and you elected to leave it as as is, stopping script."
         exit
     }
 }
 
-$AzureTenantId = Get-ParameterValue -originalParameterValue $AzureTenantId -parameterName "the ID (GUID) of the Azure tenant you wish to connect to.  See https://microsoft.github.io/AzureTipsAndTricks/blog/tip153.html on how to get that id."
+$AzureTenantId = Get-ParameterValue -originalParameterValue $AzureTenantId -parameterName "the ID (GUID) of the Azure tenant you wish to connect to.  See https://microsoft.github.io/AzureTipsAndTricks/blog/tip153.html on how to get that id"
 $AzureSubscriptionName = Get-ParameterValue -originalParameterValue $AzureSubscriptionName -parameterName "the name of the subscription you wish to connect Octopus Deploy to"
 $AzureServicePrincipalName = Get-ParameterValue -originalParameterValue $AzureServicePrincipalName -parameterName "the name of the service principal you wish to create in Azure"
 
 Write-OctopusVerbose "Logging into Azure"
 Connect-AzAccount -Tenant $AzureTenantId -Subscription $AzureSubscriptionName
 
-Write-Host "The service principal account or the Octopus Account do not exist or they haven't been hooked up.  Configuring Azure and Octopus Deploy."
+Write-OctopusVerbose "Auto-generating new password"
 $AzureServicePrincipalEndDays = Get-ParameterValue -originalParameterValue $null -parameterName "the number of days you want the service principal password to be active"
-
 $password = "$(New-Guid)$(New-Guid)" -replace "-", ""
 $securePassword = ConvertTo-SecureString $password -AsPlainText -Force
 
@@ -239,18 +327,27 @@ if ($null -eq $ExistingApplication)
     $servicePrincipal = New-AzADServicePrincipal -ApplicationId $azureAdApplication.ApplicationId
     $servicePrincipal | Format-Table
 
-    Write-OctopusSuccess "Azure Service Principal successfully created."
+    Write-OctopusSuccess "Azure Service Principal successfully created"
+    $AzureApplicationId = $azureAdApplication.ApplicationId
 }
 else 
 {
     Write-OctopusVerbose "The azue service principal $AzureServicePrincipalName already exists, creating a new password for Octopus Deploy to use."        
     New-AzADAppCredential -DisplayName "$AzureServicePrincipalName" -Password $securePassword -EndDate $endDate     
     Write-OctopusSuccess "Azure Service Principal successfully password successfully created."
+    $AzureApplicationId = $ExistingApplication.ApplicationId
 }
 
 if ($null -eq $existingAccount)
 {
-    Write-OctopusVerbose "The account $accountName does not exist, creating it"
+    Write-OctopusVerbose "Now creating the account in Octopus Deploy."
+    $tenantParticipation = "Untenanted"
+
+    if ($OctopusTenantIdList.Count -gt 0)
+    {
+        $tenantParticipation = "TenantedOrUntenanted"
+    }
+
     $jsonPayload = @{
         AccountType = "AzureServicePrincipal"
         AzureEnvironment = ""
@@ -260,18 +357,18 @@ if ($null -eq $existingAccount)
             NewValue = $password
         }
         TenantId = $AzureTenantId
-        ClientId = $azureAdApplication.ApplicationId
+        ClientId = $AzureApplicationId
         ActiveDirectoryEndpointBaseUri = ""
         ResourceManagementEndpointBaseUri = ""
-        Name = $accountName
+        Name = $OctopusAccountName
         Description = "Account created by the bootstrap script"
-        TenantedDeploymentParticipation = "Untenanted"
+        TenantedDeploymentParticipation = $tenantParticipation
         TenantTags = @()
-        TenantIds = @()
-        EnvironmentIds = @()
+        TenantIds = @($OctopusTenantIdList)
+        EnvironmentIds = @($OctopusEnvironmentIdList)
     }
 
-    Write-OctopusVerbose "Adding the just created Azure Service Principal to Octopus Deploy"
+    Write-OctopusVerbose "Adding Azure Service Principal that was just created to Octopus Deploy"    
     Invoke-OctopusApi -EndPoint "accounts" -item $jsonPayload -method "POST" -SpaceId $spaceInfo.Id -apiKey $OctopusApiKey -OctopusURL $OctopusURL
 
     Write-OctopusSuccess "Successfully added the Azure Service Principal account to Octopus Deploy"
@@ -281,5 +378,7 @@ else
     $existingAccount.Password.HasValue = $true    
     $existingAccount.Password.NewValue = $password
 
+    Write-OctopusVerbose "Updating the existing account in Octopus Deploy to use the new service principal credentials"
     Invoke-OctopusApi -EndPoint "accounts/$($existingAccount.Id)" -item $existingAccount -method "PUT" -SpaceId $spaceInfo.Id -apiKey $OctopusApiKey -OctopusURL $OctopusUrl
+    Write-OctopusSuccess "Successfully updated Azure Service Principal account in Octopus Deploy"
 }
